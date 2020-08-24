7320b0e45397c0feb0736491f83f0dc623fa07a1
Joris Coppieters <joris@trademe.co.nz>
2020-08-25 11:09:37 +1200

    Adjusted log levels

--- src/_common/ts/env/server/models/api-config.ts
+++ src/_common/ts/env/server/models/api-config.ts
@@ -18,10 +18,10 @@ export class ApiConfig {
     debugRegisterRoute: boolean;
 
     constructor(in_logLevel: number) {
-        this.debugResult = in_logLevel >= LOG_LEVEL.Info;
-        this.debugRequest = in_logLevel >= LOG_LEVEL.Info;
+        this.debugResult = in_logLevel >= LOG_LEVEL.Debug;
+        this.debugRequest = in_logLevel >= LOG_LEVEL.Debug;
         this.debugRequestUri = in_logLevel >= LOG_LEVEL.Info;
-        this.debugContext = in_logLevel >= LOG_LEVEL.Info;
+        this.debugContext = in_logLevel >= LOG_LEVEL.Debug;
         this.debugError = in_logLevel >= LOG_LEVEL.Info;
         this.debugResponse = in_logLevel >= LOG_LEVEL.Debug;
         this.debugRegisterRoute = in_logLevel >= LOG_LEVEL.Info;
